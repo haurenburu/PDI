a79fd0105b82871588ff11775c08ef5802c4b692
Antonio HaureN <haurenburu@protonmail.com>
2021-10-26 13:24:17 -0300

    fix double ; on pratica1

--- Pratica 1/pratica1.m
+++ Pratica 1/pratica1.m
@@ -6,7 +6,7 @@ im1 = zeros(256);
 im1 = uint8(im1);
 # 2 percorrer a imagem gerando um degrade de 256 tons de cinza
 for i=1:size(im1, 1)
-  im1(:,i) = i-1;;
+  im1(:,i) = i-1;
 end
 # 3 abrir uma nova janela e mostrar a imagem em degrade
 figure(1);
@@ -22,7 +22,7 @@ im2 = uint8(im2);
 
 for i=1:size(im, 1)
   for j = 1:size(im, 1)
-    # utilizar a fórmula Gray = 0.3*R +0.59*G +0.11*B
+    # utilizar a fÃ³rmula Gray = 0.3*R +0.59*G +0.11*B
     im2(i,j, 1) = 0.3 * im(i,j,1) + 0.59 * im(i,j, 2) + 0.11 * im(i, j, 3);
   end
 end
@@ -30,7 +30,7 @@ end
 figure(3);
 imshow(im2);
 
-# 6 Criar e mostrar a imagem saida1 inferindo em im2 um degradê de tal forma que a imagem varie do escuro para o claro
+# 6 Criar e mostrar a imagem saida1 inferindo em im2 um degradÃª de tal forma que a imagem varie do escuro para o claro
 for i=1:size(im2,1)
   for j = 1:size(im2,1)
     im3(i,j) = im2(i,j)-(size(im2,1)/2-j);
@@ -62,7 +62,7 @@ figure(6);
 imshow(im5);
 c2 = time();
 
-# 8 Executar novamente o passo anterior substituindo os laços utilizados por atribuições diretas
+# 8 Executar novamente o passo anterior substituindo os laÃ§os utilizados por atribuiÃ§Ãµes diretas
 c3 = time();
 im6 = im2 * 1.4;
 figure(7);
@@ -73,7 +73,7 @@ figure(8);
 imshow(im7);
 c4 = time();
 
-# 9 Contar os tempos de execução dos passos 7 e 8 e comparar os resultados.
+# 9 Contar os tempos de execuÃ§Ã£o dos passos 7 e 8 e comparar os resultados.
 disp("utilizando for")
 disp(c2-c1)
 disp("utilizando atribuicao")
